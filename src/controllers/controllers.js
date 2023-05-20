7e1db6faf19604fba229f499a1a113843650f781
curtyraissa <raissacurty@gmail.com>
2023-05-19 23:21:43 -0300

    tentar teste no hub

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -77,114 +77,114 @@ export async function login(req, res) {
 }
 
 export async function inserirURL(req, res) {
-  const { url } = req.body;
-  const { id: userId } = req.params;
+  // const { url } = req.body;
+  // const { id: userId } = req.params;
 
-  const validation = urlSchema.validate(req.body, { abortEarly: false });
+  // const validation = urlSchema.validate(req.body, { abortEarly: false });
 
-  if (validation.error) {
-    const errors = validation.error.details.map((d) => d.message);
-    return res.status(422).send(errors);
-  }
+  // if (validation.error) {
+  //   const errors = validation.error.details.map((d) => d.message);
+  //   return res.status(422).send(errors);
+  // }
 
-  // O cliente deve enviar um header de authorization com o token
-  const { authorization } = req.headers;
+  // // O cliente deve enviar um header de authorization com o token
+  // const { authorization } = req.headers;
 
-  // O formato é assim: Bearer TOKEN, então para pegar o token vamos tirar a palavra Bearer
-  const token = authorization?.replace("Bearer ", "");
+  // // O formato é assim: Bearer TOKEN, então para pegar o token vamos tirar a palavra Bearer
+  // const token = authorization?.replace("Bearer ", "");
 
-  // Se não houver token, não há autorização para continuar
-  if (!token) return res.status(401).send("Token inexistente");
+  // // Se não houver token, não há autorização para continuar
+  // if (!token) return res.status(401).send("Token inexistente");
 
-  try {
-    // Caso o token exista, precisamos descobrir se ele é válido
-    // Ou seja: se ele está na nossa collection de sessoes
-    const sessao = await db.query(`SELECT * FROM sessoes WHERE token = $1`, [token]);
-    // Verifica se há alguma sessão encontrada
-    if (sessao.rows.length === 0) return res.status(401).send("Token inválido");
+  // try {
+  //   // Caso o token exista, precisamos descobrir se ele é válido
+  //   // Ou seja: se ele está na nossa collection de sessoes
+  //   const sessao = await db.query(`SELECT * FROM sessoes WHERE token = $1`, [token]);
+  //   // Verifica se há alguma sessão encontrada
+  //   if (sessao.rows.length === 0) return res.status(401).send("Token inválido");
 
-    // Caso a sessão tenha sido encontrada, iremos guardar na variável "sessao" o objeto de sessão encontrado
-    const sessaoEncontrada = sessao.rows[0];
+  //   // Caso a sessão tenha sido encontrada, iremos guardar na variável "sessao" o objeto de sessão encontrado
+  //   const sessaoEncontrada = sessao.rows[0];
 
-    // Tendo o id do usuário, podemos procurar seus dados
-    const usuario = await db.query(`SELECT * FROM usuarios WHERE id = $1`, [sessaoEncontrada.userId]);
-    // Verifica se o usuário foi encontrado
-    if (usuario.rows.length === 0) return res.status(401).send("Usuário não encontrado");
+  //   // Tendo o id do usuário, podemos procurar seus dados
+  //   const usuario = await db.query(`SELECT * FROM usuarios WHERE id = $1`, [sessaoEncontrada.userId]);
+  //   // Verifica se o usuário foi encontrado
+  //   if (usuario.rows.length === 0) return res.status(401).send("Usuário não encontrado");
 
-    const shortUrl = nanoid(10);
+  //   const shortUrl = nanoid(10);
 
-    const inserirShortUrl = await db.query(`INSERT INTO url ("shortUrl", url, "userId") VALUES ($1, $2, $3) RETURNING id`, [shortUrl, url, userId]);
+  //   const inserirShortUrl = await db.query(`INSERT INTO url ("shortUrl", url, "userId") VALUES ($1, $2, $3) RETURNING id`, [shortUrl, url, userId]);
 
-    res.status(201).send({ "id": inserirShortUrl.rows[0].id, "shortUrl": shortUrl });
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  //   res.status(201).send({ "id": inserirShortUrl.rows[0].id, "shortUrl": shortUrl });
+  // } catch (err) {
+  //   res.status(500).send(err.message);
+  // }
 }
 
 export async function listarURLporId(req, res) {
-  const {id} = req.params
+  // const {id} = req.params
 
-  try {
-    const listaPorId = await db.query(`SELECT * FROM url WHERE id = $1;`, [id]);
+  // try {
+  //   const listaPorId = await db.query(`SELECT * FROM url WHERE id = $1;`, [id]);
 
-    if (listaPorId.rowCount === 0) {return res.sendStatus(404);}
+  //   if (listaPorId.rowCount === 0) {return res.sendStatus(404);}
 
-    res.status(200).send({"id": result.rows[0].id, "shortUrl": result.rows[0].shortUrl, "url": result.rows[0].url});
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  //   res.status(200).send({"id": result.rows[0].id, "shortUrl": result.rows[0].shortUrl, "url": result.rows[0].url});
+  // } catch (err) {
+  //   res.status(500).send(err.message);
+  // }
 }
 
 export async function buscarShortURL(req, res) {
-    const { shortUrl } = req.params;
-    try {
-     const urlExiste = await db.query(`SELECT * FROM url WHERE "shortUrl" = $1`, [shortUrl]);
-      if (urlExiste.rowCount === 0) return res.sendStatus(404)
+    // const { shortUrl } = req.params;
+    // try {
+    //  const urlExiste = await db.query(`SELECT * FROM url WHERE "shortUrl" = $1`, [shortUrl]);
+    //   if (urlExiste.rowCount === 0) return res.sendStatus(404)
 
-      await db.query(`UPDATE url SET "visitCount" = "visitCount" + 1 WHERE id = $1`, [urlExiste.rows[0].id])
+    //   await db.query(`UPDATE url SET "visitCount" = "visitCount" + 1 WHERE id = $1`, [urlExiste.rows[0].id])
       
-      res.redirect(urlExiste.rows[0].url);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+    //   res.redirect(urlExiste.rows[0].url);
+    // } catch (err) {
+    //   res.status(500).send(err.message);
+    // }
   }
 
 export async function deletarURL(req, res) {
-  const {id} = req.params
+  // const {id} = req.params
 
-  // O cliente deve enviar um header de authorization com o token
-  const { authorization } = req.headers;
+  // // O cliente deve enviar um header de authorization com o token
+  // const { authorization } = req.headers;
 
-  // O formato é assim: Bearer TOKEN, então para pegar o token vamos tirar a palavra Bearer
-  const token = authorization?.replace("Bearer ", "");
+  // // O formato é assim: Bearer TOKEN, então para pegar o token vamos tirar a palavra Bearer
+  // const token = authorization?.replace("Bearer ", "");
 
-  // Se não houver token, não há autorização para continuar
-  if (!token) return res.status(401).send("Token inexistente");
+  // // Se não houver token, não há autorização para continuar
+  // if (!token) return res.status(401).send("Token inexistente");
 
-  try {
-    // Caso o token exista, precisamos descobrir se ele é válido
-    // Ou seja: se ele está na nossa collection de sessoes
-    const sessao = await db.query(`SELECT * FROM sessoes WHERE token = $1`, [token]);
-    // Verifica se há alguma sessão encontrada
-    if (sessao.rows.length === 0) return res.status(401).send("Token inválido");
+  // try {
+  //   // Caso o token exista, precisamos descobrir se ele é válido
+  //   // Ou seja: se ele está na nossa collection de sessoes
+  //   const sessao = await db.query(`SELECT * FROM sessoes WHERE token = $1`, [token]);
+  //   // Verifica se há alguma sessão encontrada
+  //   if (sessao.rows.length === 0) return res.status(401).send("Token inválido");
 
-    // Caso a sessão tenha sido encontrada, iremos guardar na variável "sessao" o objeto de sessão encontrado
-    const sessaoEncontrada = sessao.rows[0];
+  //   // Caso a sessão tenha sido encontrada, iremos guardar na variável "sessao" o objeto de sessão encontrado
+  //   const sessaoEncontrada = sessao.rows[0];
 
-    // Tendo o id do usuário, podemos procurar seus dados
-    const usuario = await db.query(`SELECT * FROM usuarios WHERE id = $1`, [sessaoEncontrada.userId]);
-    // Verifica se o usuário foi encontrado
-    if (usuario.rows.length === 0) return res.status(401).send("Usuário não encontrado");
+  //   // Tendo o id do usuário, podemos procurar seus dados
+  //   const usuario = await db.query(`SELECT * FROM usuarios WHERE id = $1`, [sessaoEncontrada.userId]);
+  //   // Verifica se o usuário foi encontrado
+  //   if (usuario.rows.length === 0) return res.status(401).send("Usuário não encontrado");
 
-    const urlUsuario = await db.query(`SELECT * FROM url WHERE id = $1`, [id])
-    if (urlUsuario.rowCount === 0) return res.sendStatus(404)
+  //   const urlUsuario = await db.query(`SELECT * FROM url WHERE id = $1`, [id])
+  //   if (urlUsuario.rowCount === 0) return res.sendStatus(404)
 
-    await db.query(`DELETE FROM url WHERE id = $1`, [id])
+  //   await db.query(`DELETE FROM url WHERE id = $1`, [id])
 
-  res.sendStatus(204);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  // res.sendStatus(204);
+  // } catch (err) {
+  //   res.status(500).send(err.message);
+  // }
 }
 
 export async function listarUsuarioToken(req, res) {
